a1c234c9a5323e421f342e69919ea60771761335
First name hg <cistakovvasilijaleksandrovic@192.168.1.8>
2021-11-20 20:22:27 +0300

    20.11

---
+++ LoginPas/AuthViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AuthViewController.swift
+//  LoginPas
+//
+//  Created by Чистяков Василий Александрович on 20.11.2021.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var enterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func switchLogin(_ sender: Any) {
+        
+        
+    }
+    
+}
---
+++ LoginPas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginPas
+//
+//  Created by Чистяков Василий Александрович on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
